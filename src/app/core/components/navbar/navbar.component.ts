b57428cc96fdd02c18d208d8a3e512e70a8e92a9
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2023-01-03 08:42:39 +0100

    NavBar fonctionnelle

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'al-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  homePath: string = 'home';
+  loginPath: string = 'login';
+  registerPath: string = 'register';
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public isActive(page: string): boolean {
+    return this.router.isActive(page, true);
+  }
+
+  public navigate(page: string): void {
+    this.router.navigate([page]);
   }
 
 }
