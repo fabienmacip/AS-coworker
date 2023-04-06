6cd2c14558d6eddaa732294515911312821764e6
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2023-04-06 11:18:58 +0200

    Validateurs

--- src/app/protected/workday/workday-form/workday-form.component.ts
+++ src/app/protected/workday/workday-form/workday-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormArray, FormBuilder, FormControl, FormGroupDirective } from '@angular/forms';
+import { FormGroup, FormArray, FormBuilder, FormControl, FormGroupDirective, Validators } from '@angular/forms';
 
 @Component({
   selector: 'al-workday-form',
@@ -30,9 +30,16 @@ export class WorkdayFormComponent implements OnInit {
 
   createWorkdayForm(): FormGroup {
    const workdayForm: FormGroup = this.fb.group({
-    'dueDate': '',
-    'tasks': this.fb.array([]),
-    'notes': '',
+    'dueDate': ['', [
+      Validators.required
+    ]],
+    'tasks': this.fb.array([], [
+      Validators.required,
+      Validators.maxLength(6)
+    ]),
+    'notes': ['',[
+      Validators.maxLength(1000)
+    ]],
    });
 
    return workdayForm;
