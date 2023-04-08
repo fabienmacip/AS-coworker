8b0f5f4d87585f8f98971e079e517dc5f945fba5
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2023-04-08 11:54:09 +0200

    Le service de retour utilisateur, ou toastr

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,7 +7,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { AlertModule } from 'ngx-bootstrap/alert';
 import { LoaderComponent } from './components/loader/loader.component';
+import { ToastrComponent } from './components/toastr/toastr.component';
 
 
 @NgModule({
@@ -15,19 +17,22 @@ import { LoaderComponent } from './components/loader/loader.component';
     NavbarComponent,
     FooterComponent,
     PageNotFoundComponent,
-    LoaderComponent
+    LoaderComponent,
+    ToastrComponent
   ],
   imports: [
     CommonModule,
     BrowserAnimationsModule,
     PublicModule,
-    ProtectedModule
+    ProtectedModule,
+    AlertModule.forRoot(),
   ],
   exports: [
     NavbarComponent,
     FooterComponent,
     PageNotFoundComponent,
-    LoaderComponent
+    LoaderComponent,
+    ToastrComponent
   ]
 })
 export class CoreModule {
